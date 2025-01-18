0517380620ae93e81c6df458a480f5c0d598a253
Jacob Isaacson <jisaacs46@gmail.com>
2025-01-17 20:47:12 -0500

    finished chapter 2

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import pygame
+from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        for obj in updatable:
+            obj.update(dt)
+                        
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
